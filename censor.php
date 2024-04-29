820d1e9cb30a6ed163458d4fd7c4cbe20ce1f435
DavideAlovisio13 <alovisiod@gmail.com>
2024-04-29 15:42:47 +0200

    Refactor censor.php to include bad word filtering, display original text, and calculate length

--- censor.php
+++ censor.php
@@ -4,6 +4,7 @@
     $UpperCaseFirst = ucfirst($text);
     $censoredText = str_replace($badWords, '***', $UpperCaseFirst);
     $stringLenght = strlen($censoredText); 
+    $responseFrank = 'a volte mi sento un povero poliziotto, in un modo di stronzi!'
 ?>
 
 <!DOCTYPE html>
@@ -15,12 +16,20 @@
     <title>censor</title>
 </head>
 <body>
+    <h1>Ah! Ah! Ah! che parole !?</h1>  
     <div class="container">
-        <img src="img/15144211.jpg" alt="frank">
+        <img src="img/15144211.jpg" alt="frank" id="frank1">
         <div class="text">
-            <p>Testo censurato: <? echo $censoredText ?></p>
-            <p>Testo originale: <? echo $UpperCaseFirst ?></p>
-            <p>Lunghezza: <? echo $stringLenght ?></p>
+            <p><strong>Testo censurato:</strong> "<? echo $censoredText ?>"</p>
+            <p><strong>Testo originale:</strong> "<? echo $UpperCaseFirst ?>"</p>
+            <p><strong>Lunghezza:</strong> "<? echo $stringLenght ?>"</p>
+        </div>
+    </div>
+
+    <div class="container">
+        <img src="img/the-naked-gun-from-the-files-of-police-squad-feature.webp" alt="frank-smile" id="frank2">
+        <div class="text">
+            <p><strong>Frank:</strong>"<? echo $responseFrank ?>"</p>
         </div>
     </div>
 </body>
--- index.php
+++ index.php
@@ -9,25 +9,28 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/style_index.scss">
     <title>BadWords</title>
 </head>
 <body>
     <div class="title">
+        <img src="./img/Vector-Bullet-Hole-Graphics-72102740-1.png" alt="bullets-holes">
         <h1>BadWords</h1>
+        <img src="./img/Vector-Bullet-Hole-Graphics-72102740-1.png" alt="bullets-holes">
     </div>
     <div class="form">
         <form action="censor.php" method="GET">
             <input type="text" name="text">
-            <input type="submit">
+            <input type="submit" value="Spedisci a Frank">
         </form>
     </div>
-    <div>
+    <div class="examples">
         <h2>Esempi da poter utilizzare:</h2>
-        <ul>
+        <ol>
             <li><?php echo $textExample ?></li>
             <li><?php echo $textExample2 ?></li>
             <li><?php echo $textExample3 ?></li>
-        </ul>
+        </ol>
     </div>
 
 </body>
